ce4e18d30ca10c9dd26e1985d94dcd0433301605
Localinst <giuseppetotirebusiness@gmail.com>
2026-02-19 17:41:13 +0100

    H

--- src/pages/Supporto.tsx
+++ src/pages/Supporto.tsx
@@ -249,10 +249,7 @@ const SupportoPage = () => {
                 )}
 
                 <div className="mt-8 space-y-4">
-                  <div className="flex items-center gap-3">
-                    <Phone className="h-5 w-5 text-primary" />
-                    <span>+393663958461</span>
-                  </div>
+                  
                   <div className="flex items-center gap-3">
                     <Mail className="h-5 w-5 text-primary" />
                     <span>tenoris360help@gmail.com</span>
